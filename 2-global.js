aea379fe9f13a0f0e39c0f7d28057d100b13250d
jakedenley <denley420@gmail.com>
2022-09-16 09:36:19 +0800

    first commit

---
+++ 2-global.js
@@ -0,0 +1,11 @@
+// __dirname    - path to directory
+// __filename   - file name
+// require      - function to use modules (commonJS)
+// module       - info about current module (file)
+// process      - info about env where the program is being executed
+
+console.log(__filename);
+
+setInterval(() => {
+    console.log('hello')
+}, 500)
---
+++ 4-names.js
@@ -0,0 +1,7 @@
+// local
+const secret = 'SUPER SECRET'
+// share
+const jake = 'jake'
+const jed = 'jed'
+
+module.exports = { jake, jed }
